e6401ec133ecb181627ff8b5f87493dde6303c24
Hans Jacob Westbye <39839150+hjwestbye@users.noreply.github.com>
2024-01-09 14:40:55 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -28543,7 +28543,7 @@ const Header = ()=>{
             display: "flex",
             justifyContent: "center",
             gap: "2rem",
-            background: "rgba(255,255,255,0.75)",
+            background: "#c39f78",
             padding: "1rem",
             top: 0,
             width: "100%",
